54a54fe39151d43a3b756eaefda96e17391b6d5e
Tadeu Mendonça <tadeu.mendonca@g.globo>
2020-09-30 20:53:01 -0300

    Revert "Change"
    This reverts commit 9f6cfae739d78833f0ba6d256141fa4f49698cb9.

--- pages/index.js
+++ pages/index.js
@@ -5,13 +5,13 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Tadeu Next App</title>
+        <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to Tadeu's Next.js App
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className={styles.description}>
